032f57a0191cc6a1e690dc5c3fe4b621230279b0
spemer <ghsspower@gmail.com>
2019-07-30 05:51:49 +0900

    FIX share api fallback

--- src/mixins/shareApi.js
+++ src/mixins/shareApi.js
@@ -7,6 +7,34 @@ import {
 
 export const shareApi = {
   methods: {
+    copyTextToClipboard(text) {
+      let textArea = document.createElement('textarea')
+      textArea.style.position = 'fixed'
+      textArea.style.top = 0
+      textArea.style.left = 0
+      textArea.style.width = '2em'
+      textArea.style.height = '2em'
+      textArea.style.padding = 0
+      textArea.style.border = 'none'
+      textArea.style.outline = 'none'
+      textArea.style.boxShadow = 'none'
+      textArea.style.background = 'transparent'
+      textArea.value = text
+      document.body.appendChild(textArea)
+      textArea.focus()
+      textArea.select()
+
+      try {
+        let successful = document.execCommand('copy')
+        let msg = successful ? 'successful' : 'unsuccessful'
+        console.log('Copying text command was ' + msg)
+      } catch (err) {
+        console.log('Oops, unable to copy')
+      }
+
+      document.body.removeChild(textArea)
+    },
+
     shareApi() {
       if (navigator.share) {
         navigator.share({
@@ -15,16 +43,11 @@ export const shareApi = {
           url: 'https://sprinter-web.web.app',
         })
       } else if (!navigator.share) {
-        navigator.clipboard
-          .writeText(globalVar.url)
-          .then(() => {
-            this.toast('URL copied to clipboard!', 2500, this.$t('close'))
-          })
-          .catch((err) => {
-            console.error(`Could not copy text: ${err}`)
-          })
+        this.copyTextToClipboard(globalVar.url)
+        this.toast('URL copied to clipboard!', 2500, this.$t('close'))
       }
     },
+
   },
 
   mixins: [
